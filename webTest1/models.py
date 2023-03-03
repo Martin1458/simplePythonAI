99138fadccd69bb1f689527d3569dc4a9f23ade5
Martin1458 <martinisbored1@gmail.com>
2023-03-03 17:36:36 +0100

    fixed path to model, but on win10 still not working

--- webTest1/models.py
+++ webTest1/models.py
@@ -5,15 +5,16 @@ import pickle
 script_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(script_dir)
 
-print(script_dir)
-print(parent_dir)
+#print(script_dir)
+#print(parent_dir)
 
-models_folder = os.path.join(parent_dir, "preTrainedModels/")
+models_folder = os.path.join(parent_dir, "preTrainedModels")
 
 # Assume `X_new` is a new dataset you want to make predictions on
 #y_pred = model.predict(X_new)
 
 def load_model(model_file_name):
+    print(os.path.join(models_folder, model_file_name))
     with open(os.path.join(models_folder, model_file_name), "rb") as f:
         model = pickle.load(f)
     print("Model type"+str(type(model)))
