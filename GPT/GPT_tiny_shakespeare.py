7e7a94ba7ced018b2f1043690165a6e0a06a32e8
Martin1458 <martinisbored1@gmail.com>
2023-04-03 11:22:48 +0200

    converting to linux

--- GPT/GPT_tiny_shakespeare.py
+++ GPT/GPT_tiny_shakespeare.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-# hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+#
+batch_size = 32
+block_size = 8
 max_iters = 3000
 eval_interval = 300
 learning_rate = 1e-2
@@ -13,7 +13,7 @@ eval_iters = 200
 # ------------
 
 # Read our shakespeare dataset
-with open(r"GPT\datasets\tinyshakespeare.txt", "r", encoding="UTF-8") as f:
+with open(r"GPT/datasets/tinyshakespeare.txt", "r", encoding="UTF-8") as f:
     text = f.read()
 
 
